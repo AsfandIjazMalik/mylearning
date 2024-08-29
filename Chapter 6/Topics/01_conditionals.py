acfe1d8ee73d2dc754263d78018daecc0bd46b19
Asfand Ijaz Malik <asfandijazmalik@gmail.com>
2024-08-29 23:24:35 +0500

    Create 01_conditionals.py

---
+++ Chapter 6/Topics/01_conditionals.py
@@ -0,0 +1,20 @@
+print("If Statement")
+
+# if Else Statement
+# Taking user input
+age = int(input("Enter your age :")) 
+
+# Check the entered age by user if it is greater or equal to 18
+if (age>=18):
+    print("You Enter your ages is:", age)
+    print("You are Eligible")
+
+# iF above condition is not match else block will print
+else:
+    print("You Enter your ages is:", age)
+    print("You are not Eligible")
+
+# It will Execute after if-Else Block
+print("THANK YOU!!!")
+
+# All these connected with each other cheching one by one which
