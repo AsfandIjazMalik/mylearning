dfa49f8543e40c8bd2acea092440ebdbe60f35e4
Asfand Ijaz Malik <asfandijazmalik@gmail.com>
2024-09-13 18:55:05 +0500

    Create 01_Loops.py

---
+++ Chapter 7/Topics/01_Loops.py
@@ -0,0 +1,7 @@
+# In Python, loops are used to execute a block of
+# code repeatedly as long as a certain condition is met.
+
+# The two primary types of loops in Python are:
+
+# for Loop: Iterates over a sequence (like a list, tuple, string, or range).
+# while Loop: Repeats as long as a given condition is True.
