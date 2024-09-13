0f6dc6a00b64e22d7d5e62933e9562050ca55a4e
Asfand Ijaz Malik <asfandijazmalik@gmail.com>
2024-09-13 19:02:58 +0500

    Create 03_functions_with_arguments.py

---
+++ Chapter 8/Topics/03_functions_with_arguments.py
@@ -0,0 +1,12 @@
+# Function with arguments 
+
+def func_with_arguments(name, enddd):
+    print("Hi, " + name)
+    print(enddd)
+
+func_with_arguments("Asfand", "Thank you")
+func_with_arguments("Ijaz", "Thank you")
+func_with_arguments("Malik", "Thank you\n")
+
+# Return Value Concept
+# Function have return value also
