49a4fb63c3f4ea84cb00c2ac218642a09f0eb5d3
Asfand Ijaz Malik <asfandijazmalik@gmail.com>
2024-09-13 18:55:15 +0500

    Create 04_Quiz_quiz.py

---
+++ Chapter 7/Topics/04_Quiz_quiz.py
@@ -0,0 +1,9 @@
+# Quick Quiz: Write a program to print the content of a list using while loops.
+
+lst = ["Asfand Ijaz Malik", "Sadaf Asfand", "Saima Shahbaz", 1,22,33,44]
+
+a = 0
+
+while(a<len(lst)):
+    print(lst[a])
+    a +=1
