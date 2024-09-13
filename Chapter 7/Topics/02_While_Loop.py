7115233dd0f4dab5b439ccf83f22050b6505cae2
Asfand Ijaz Malik <asfandijazmalik@gmail.com>
2024-09-13 18:55:08 +0500

    Create 02_While_Loop.py

---
+++ Chapter 7/Topics/02_While_Loop.py
@@ -0,0 +1,12 @@
+a = 1
+
+while(a<=10): # The block keeps executing until the condition is true 
+    print("The a is now",a) #Body of the loop
+    # increment of 1
+    a +=1  # a = a + 1
+
+# In while loops, the condition is checked first. If it evaluates to true,
+# the body of the loop is executed otherwise not!
+
+# If the loop is entered, the process of [condition check & execution] is
+# continued until the condition becomes False.
