b73a42c3f00c71e97ef1b80ca99cc33e1f65f34d
Asfand Ijaz Malik <asfandijazmalik@gmail.com>
2024-09-13 19:03:06 +0500

    Create 05_function_with_default_arguments.py

---
+++ Chapter 8/Topics/05_function_with_default_arguments.py
@@ -0,0 +1,16 @@
+# Funtion with default Parameter Values
+
+# compliment = "Your are selected" is default Parameter Values
+
+def function_with_default_value(name, compliment = "Your are selected"): 
+    print("Hi " + name)
+    print(compliment)
+
+function_with_default_value("Asfand")
+print("\n")
+
+# agr value nae di compliment ki calling k wqt to to wo
+# default value le ly ga agr value de di funciton calling
+# k wqt to wo finction call k wqt di gavalue le ga like below
+
+function_with_default_value("Asfnad" , "Your Are Selected & Please Us Tomorrow For the Job")
