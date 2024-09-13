abf0cc03968490ca64fa7b7e6ba51b1e8aff8ab6
Asfand Ijaz Malik <asfandijazmalik@gmail.com>
2024-09-13 19:02:46 +0500

    Create 02_problem.py

---
+++ Chapter 8/Practise_Sets/02_problem.py
@@ -0,0 +1,12 @@
+# Problem 02
+
+# Write a python program using function to convert Celsius to Fahrenheit.
+
+def cal_celsius_tofahrenheit():
+    cel = int(input("Enter the Celsius: "))
+    Fahrenheit = (cel * 1.8)+32
+    return Fahrenheit
+
+
+a = print("Fahrenheit is" , cal_celsius_tofahrenheit())
+print(a)
