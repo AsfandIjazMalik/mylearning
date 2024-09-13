e28ffb424caa3de0c28fdd0c3db034cb1b6f001d
Asfand Ijaz Malik <asfandijazmalik@gmail.com>
2024-09-13 19:02:53 +0500

    Create 01_functions.py

---
+++ Chapter 8/Topics/01_functions.py
@@ -0,0 +1,13 @@
+# Functions in Python
+
+# Function Defination
+def average_of_num():
+    a = int(input("Enter 1st Num: "))
+    b = int(input("Enter 2nd Num: "))
+    c = int(input("Enter 3rd Num: "))
+
+    avg = (a+b+c)/3
+    print("Average of 3 Numbers is",avg)
+
+# Funcitons call
+average_of_num()
