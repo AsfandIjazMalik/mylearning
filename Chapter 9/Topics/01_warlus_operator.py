a58cba1c70f8abddfb4cdca2dd5abb59a375ff48
Asfand Ijaz Malik <asfandijazmalik@gmail.com>
2024-11-03 14:56:00 +0500

    Create 01_warlus_operator.py

---
+++ Chapter 9/Topics/01_warlus_operator.py
@@ -0,0 +1,27 @@
+# Warlus Operator introduceed in 3.8 version of Python
+# Convience method to do 2 work in one line one of code 
+# := is symbol of warlus operator
+
+# Example of Walar operator
+if (n := len([1,2,34])) >= 3:
+    print("Yes Lenght is equal to 3")
+else:
+    print("NO")
+
+
+# Without warlus operator 
+friend_names_list = []
+
+while True:
+    friends_name = input("Enter the Name of the friends how many you want: ")
+    friend_names_list.append(friends_name)
+    if friends_name == 'quit':
+        print(friend_names_list)
+        break
+
+# Above example With warlus operator
+
+new_friend_names_list = []
+while (name := input("Enter the Name of the friends how many you want: ")) != 'quit':
+    new_friend_names_list.append(name)
+
