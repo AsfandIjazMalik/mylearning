a701805bf5f8cff73040acf65e529f0084f4c673
Asfand Ijaz Malik <asfandijazmalik@gmail.com>
2024-11-09 10:07:20 +0500

    Create 13_join methods.py

---
+++ Chapter 9/Topics/13_join methods.py
@@ -0,0 +1,7 @@
+# Join Method works for strings
+
+lst = ['Asfand', 'Ijaz', 'Malik']
+print(type(lst))
+join_method = '\n'.join(lst) # can join with . ? + number letter or any thing
+print(join_method)
+print(type(join_method))
