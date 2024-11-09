7d816ed642f36d1a2ac07454a240e31104ce0c9e
Asfand Ijaz Malik <asfandijazmalik@gmail.com>
2024-11-09 10:07:33 +0500

    Create 14_formate.py

---
+++ Chapter 9/Topics/14_formate.py
@@ -0,0 +1,6 @@
+# # F strring in version Python 3.6 is advance verrsion of formate and use of formate is decrease 
+# formate can be seen in old source code of python For most cases in modern Python (3.6+),
+# f-strings are the go-to choice, but format() is still available for compatibility and specific use cases.
+
+formate_method  = '{} is my name and {} is my age'.format('Asfand Ijaz Malik', 31.5)
+print(formate_method)
