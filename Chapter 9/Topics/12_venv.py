1beb8b31e4860e7d429cac4149d3f74e35412271
Asfand Ijaz Malik <asfandijazmalik@gmail.com>
2024-11-09 10:07:15 +0500

    Create 12_venv.py

---
+++ Chapter 9/Topics/12_venv.py
@@ -0,0 +1,38 @@
+# What is  Virtual Envirement?
+# > An environment which is same as the system interpreter but is isolated from the other Python environments on the system.
+# > We need virtual environments to keep each project’s packages separate. This way:
+# > No Conflicts: Different projects can use different package versions.
+# > Clean Setup: Only the needed packages are installed for each project.
+# > Easier Sharing: Others can run your project with the same setup.
+# > It makes projects simpler to manage and avoids problems
+
+# To install the virtual environment tool in Python, you can use this command:
+# 		pip install virtualenv
+# To create envirement,  you can use this command:
+# 	            virtualenv myname (name kch b rkh skty jo ap chahty)To activate my virtual envirement
+#                         myenvirementname\Scripts\activate
+# Activate hone se phly Command Promt me asy dekht ga
+
+# Virtual Enviremnt Install Hone k bad asy dekhy ga with env its mean It now activated
+
+# VS CODE me env nae dekhay de ga but hmne activate kia o ga envirement to wo usi me sb kch 
+# install kry ga
+# PIP FREEZE COMMAND
+# ‘The command pip freeze lists all the installed packages and their versions in your current environment.. 
+# 		pip freeze
+# saves a list of all the packages and their versions in your virtual environment to a file called requirements.txt.
+# 		pip freeze > requirements.txt
+# Share Dependencies: You can share this file with others so they know exactly which packages to install to run your project.
+# Easily Recreate the Environment: Anyone can use pip install -r requirements.txt to install all the same packages in a new environment, ensuring the project runs consistently.
+
+# We can distribute this file to other users, and they can recreate the same environment using:
+
+# 		pip install –r requirements.txt
+# By using this commands we can intasll all package of requirement. txt file at once. no one by
+# one
+
+
+
+
+
+
